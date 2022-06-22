ae34193231ceecaf2edb9923934710828451784d
wolsos-v <mamaev757@gmail.com>
2022-06-22 18:41:24 +0300

    init: start remote-control task

---
+++ src/http_server/index.ts
@@ -0,0 +1,17 @@
+import { readFile } from 'fs';
+import { resolve, dirname } from 'path';
+import { createServer } from 'http';
+
+export const httpServer = createServer((req, res) => {
+  const __dirname = resolve(dirname(''));
+  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+  readFile(file_path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
