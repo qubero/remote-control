20d41cbf85c91d4189808af8a347cdf8d9ac4aad
wolsos-v <mamaev757@gmail.com>
2022-06-25 00:27:44 +0300

    feat: rewrite server using streams

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,17 +1,30 @@
-import { readFile } from 'fs';
+import { createReadStream } from 'fs';
 import { resolve, dirname } from 'path';
 import { createServer } from 'http';
+import { pipeline } from 'stream/promises';
 
-export const httpServer = createServer((req, res) => {
-  const __dirname = resolve(dirname(''));
-  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-  readFile(file_path, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
+enum HttpCode {
+  OK = 200,
+  NotFound = 404,
+}
+
+const ContentType = {
+  TEXT: { 'Content-Type': 'text/html; charset=utf-8' },
+  JSON: { 'Content-Type': 'application/json' },
+};
+
+export const httpServer = createServer(async (req, res) => {
+  try {
+    const __dirname = resolve(dirname(''));
+    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+
+    res.writeHead(HttpCode.OK, ContentType.TEXT);
+    await pipeline(
+      createReadStream(file_path),
+      res
+    );
+  } catch (err) {
+    res.writeHead(HttpCode.NotFound, ContentType.JSON);
+    res.end(JSON.stringify(err));
+  }
 });
